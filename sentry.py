797913a4eb7596e0ee92e5c97629229698c91e9b
Eric Hilary Smith <erichilarysmithsr@gmail.com>
2023-02-21 09:18:17 -0600

    Update sentry.py

--- sentry.py
+++ sentry.py
@@ -1,1 +1,7 @@
-
+def middleware(request, response):
+    response['Public-Key-Pins'] = \
+        'pin-sha256="cUPcTAZWKaASuYWhhneDttWpY3oBAkE3h2+soZS7sWs="; ' \
+        'pin-sha256="M8HztCzM3elUxkcjR2S5P4hhyBNf6lHkmjAHKhpGPWE="; ' \
+        'max-age=5184000; includeSubDomains; ' \
+        'report-uri="https://o229048.ingest.sentry.io/api/4503937007157248/security/?sentry_key=a35a59ffdbb24b90a82df3d8c5e33494"' 
+    return response
