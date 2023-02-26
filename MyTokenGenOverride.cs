73856c5696a2170f1700d95b8e76bcb6fb536c68
Eric Hilary Smith <erichilarysmithsr@gmail.com>
2023-02-26 12:38:12 -0600

    Update MyTokenGenOverride.cs

--- MyTokenGenOverride.cs
+++ MyTokenGenOverride.cs
@@ -1,1 +1,14 @@
+```
+public class MyTokenGenOverride : VxTokenGen
+{
+    public override string GetToken(string issuer = null, TimeSpan? expiration = null, string userUri = null, string action = null, string tokenKey = null, string conferenceUri = null, string fromUserUri = null)
+    {
+        //Server Token Generation calls go here
+    }
+}
 
+public void SetTokenOverride()
+{
+    Client.tokenGen = new MyTokenGenOverride();
+}
+```
