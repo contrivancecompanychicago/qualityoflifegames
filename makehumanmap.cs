7fb7a4d869e4c0f45e734a475637e8e516ef8772
Eric Hilary Smith <erichilarysmithsr@gmail.com>
2023-03-01 17:00:13 -0600

    Add files via upload

---
+++ makehumanmap.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+namespace __yellowmubu
+{
+    #region Makehumanmap
+    public class Makehumanmap
+    {
+        #region Member Variables
+        protected string _COL;
+        protected string _COL;
+        protected string _COL;
+        #endregion
+        #region Constructors
+        public Makehumanmap() { }
+        public Makehumanmap(string COL, string COL, string COL)
+        {
+            this._COL=COL;
+            this._COL=COL;
+            this._COL=COL;
+        }
+        #endregion
+        #region Public Properties
+        public virtual string COL
+        {
+            get {return _COL;}
+            set {_COL=value;}
+        }
+        public virtual string COL
+        {
+            get {return _COL;}
+            set {_COL=value;}
+        }
+        public virtual string COL
+        {
+            get {return _COL;}
+            set {_COL=value;}
+        }
+        #endregion
+    }
+    #endregion
+}
