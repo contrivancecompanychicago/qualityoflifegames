c545679fc6c23d80f9e5a2122bccb0feddc1a17e
Eric Hilary Smith <erichilarysmithsr@gmail.com>
2023-03-02 12:24:57 -0600

    Update HostPopulation.java

--- HostPopulation.java
+++ HostPopulation.java
@@ -9,7 +9,7 @@ import org.tmatesoft.sqljet.core.SqlJetTransactionMode;
 import org.tmatesoft.sqljet.core.table.*;
 
 
-public class HostPopulation extends Civlization {
+public class HostPopulation extends Character {
 	private Simulation sim;
 	private Parameters params;
 	private Phenotype urImmunity;
