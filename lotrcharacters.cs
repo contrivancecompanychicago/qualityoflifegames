d834227d7215756c90f63472e5ff02229393dce4
Eric Hilary Smith <erichilarysmithsr@gmail.com>
2023-03-02 08:31:40 -0600

    Add files via upload

---
+++ lotrcharacters.cs
@@ -0,0 +1,98 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+namespace __yellowmubu
+{
+    #region Lotrcharacters
+    public class Lotrcharacters
+    {
+        #region Member Variables
+        protected string _COL;
+        protected string _COL;
+        protected string _COL;
+        protected string _COL;
+        protected string _COL;
+        protected string _COL;
+        protected string _COL;
+        protected string _COL;
+        protected string _COL;
+        protected string _COL;
+        protected string _COL;
+        #endregion
+        #region Constructors
+        public Lotrcharacters() { }
+        public Lotrcharacters(string COL, string COL, string COL, string COL, string COL, string COL, string COL, string COL, string COL, string COL)
+        {
+            this._COL=COL;
+            this._COL=COL;
+            this._COL=COL;
+            this._COL=COL;
+            this._COL=COL;
+            this._COL=COL;
+            this._COL=COL;
+            this._COL=COL;
+            this._COL=COL;
+            this._COL=COL;
+        }
+        #endregion
+        #region Public Properties
+        public virtual string COL
+        {
+            get {return _COL;}
+            set {_COL=value;}
+        }
+        public virtual string COL
+        {
+            get {return _COL;}
+            set {_COL=value;}
+        }
+        public virtual string COL
+        {
+            get {return _COL;}
+            set {_COL=value;}
+        }
+        public virtual string COL
+        {
+            get {return _COL;}
+            set {_COL=value;}
+        }
+        public virtual string COL
+        {
+            get {return _COL;}
+            set {_COL=value;}
+        }
+        public virtual string COL
+        {
+            get {return _COL;}
+            set {_COL=value;}
+        }
+        public virtual string COL
+        {
+            get {return _COL;}
+            set {_COL=value;}
+        }
+        public virtual string COL
+        {
+            get {return _COL;}
+            set {_COL=value;}
+        }
+        public virtual string COL
+        {
+            get {return _COL;}
+            set {_COL=value;}
+        }
+        public virtual string COL
+        {
+            get {return _COL;}
+            set {_COL=value;}
+        }
+        public virtual string COL
+        {
+            get {return _COL;}
+            set {_COL=value;}
+        }
+        #endregion
+    }
+    #endregion
+}
