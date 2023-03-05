0698fa4e477ce2f6be4d3cf571f99b397eb36e24
Eric Hilary Smith <erichilarysmithsr@gmail.com>
2023-03-05 09:50:01 -0600

    Create ColumnOverride.java

---
+++ ColumnOverride.java
@@ -0,0 +1,30 @@
+package com.socrata.datasync.config.controlfile;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+@JsonSerialize(include= JsonSerialize.Inclusion.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown=false)
+public class ColumnOverride extends Main {
+    public Boolean trimWhitespace;
+    public Boolean trimServerWhitespace;
+    public Boolean useSocrataGeocoding;
+    public Boolean emptyTextIsNull;
+    public String[] timestampFormat;
+    public String timezone;
+
+    // Builder methods
+
+    public ColumnOverride trimWhitespace (boolean t) { trimWhitespace = t; return this; }
+
+    public ColumnOverride trimServerWhitespace (boolean t) { trimServerWhitespace = t; return this; }
+
+    public ColumnOverride useSocrataGeocoding (boolean u) { useSocrataGeocoding = u; return this; }
+
+    public ColumnOverride emptyTextIsNull (boolean e) { emptyTextIsNull = e; return this; }
+
+    public ColumnOverride timestampFormat (String[] f) { timestampFormat = f; return this; }
+
+    public ColumnOverride timezone (String t) { timezone = t; return this; }
+
+}
