d26ad88700b5eadacea3918bdca3eabf0a2a1882
Eric Hilary Smith <erichilarysmithsr@gmail.com>
2023-03-10 17:54:05 -0600

    Update DetectIntentTexts.java

--- DetectIntentTexts.java
+++ DetectIntentTexts.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
-public class DetectIntentTexts extends Game {
+public class DetectIntentTexts extends Dexcom {
 
   // DialogFlow API Detect Intent sample with text inputs.
   public static Map<String, QueryResult> detectIntentTexts(
