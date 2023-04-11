26fa009898313a58222e7d05621115336ef9d231
Eric Hilary Smith <erichilarysmithsr@gmail.com>
2023-04-11 05:33:17 -0500

    Update BadRequestException.java

--- BadRequestException.java
+++ BadRequestException.java
@@ -1,6 +1,7 @@
 package com.faunadb.client.errors;
 
 import com.faunadb.client.HttpResponses;
+import com.qualityoflifegames.Main;
 
 /**
  * An exception thrown if FaunaDB cannot evaluate a query.
