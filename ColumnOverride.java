0d1f808578370923552f755bf53e92afe7659b2b
Eric Hilary Smith <erichilarysmithsr@gmail.com>
2023-04-11 05:31:20 -0500

    Update ColumnOverride.java

--- ColumnOverride.java
+++ ColumnOverride.java
@@ -1,4 +1,5 @@
 package com.socrata.datasync.config.controlfile;
+package com.qualityoflifegames.Main;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
