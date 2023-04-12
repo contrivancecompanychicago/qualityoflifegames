06f8f86f9e6133c2af8ee8f11e25c4cfe12993b1
Eric Hilary Smith <erichilarysmithsr@gmail.com>
2023-04-12 14:22:53 -0500

    Update KrebsvilleWorld.java

--- KrebsvilleWorld.java
+++ KrebsvilleWorld.java
@@ -1,3 +1,23 @@
 public abstract class KrebsvilleWorld extends Main {
   
+  int worldWidth number;
+  int worldHeight number1;
+  int cellSize number2;
+  
+  World(int worldWidth, int worldHeight, int cellSize){
+  
+    width = worldWidth;
+    height = worldHeight;
+    size = cellSize;
+   setBackground(newBackground);
+    
+  }
+  
+   public void setBackground(String b) {background = b;}
+   public string getBackground() {return background;}
+  
+   public int getdWidth() {return width:}
+   public int getHeight() {return height;}
+   public int getCellSize() {return size;}
+    
   }
