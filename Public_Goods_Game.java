f33d1edf8ee0c8a5f938625df3656d134872b068
Eric Hilary Smith <erichilarysmithsr@gmail.com>
2023-04-11 19:59:23 -0500

    Update Public_Goods_Game.java

--- Public_Goods_Game.java
+++ Public_Goods_Game.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package public_goods_game;
+package com.qualityoflifegames.Main;
 
 import java.util.Scanner;
 
