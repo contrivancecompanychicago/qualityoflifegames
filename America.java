1864d355ba1f5100fcbf40f07c4a87b5cda999fd
Eric Hilary Smith <erichilarysmithsr@gmail.com>
2023-04-13 17:32:07 -0500

    Update America.java

--- America.java
+++ America.java
@@ -2,7 +2,7 @@ package civ.core.map.civilization;
 
 import java.awt.Color;
 
-public class America extends BaseCivilization {
+public class America extends Landscape {
   public America() {
     super("America", "American", new Color(34, 103, 214),
         new String[] {"Washington", "New York", "Boston", "Philadelphia", "Atlanta", "Chicago",
