ea896aeba506e586f138c60127d287808a8d7f61
Eric Hilary Smith <erichilarysmithsr@gmail.com>
2023-04-13 20:33:38 -0500

    Update UnityPlayerActivity.java

--- UnityPlayerActivity.java
+++ UnityPlayerActivity.java
@@ -1,3 +1,25 @@
 public class UnityPlayerActivity extends Main
 {
+
+int capacity number;
+
+protected void onStart();
+  
+PersistableBundle(int capacity){
+  
+  capacity = Capacity;
+  
+  setPersistableBundle(newPersistableBundle);
+  
 }
+
+public void setPeristableBundle(string key) {key = key;}
+  public string getPersistableBundle() {return key;}
+
+ public void putPersistableBundle(string key, PersistableBundle value) {key = key, value = value;}
+ 
+ public Object clone() {return object;}
+ 
+}
+
+
